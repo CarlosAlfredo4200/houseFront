df71b7875a5ba8242f6f3e6cf4f96fb7d10b63d2
Carlos <alfredomontoyagit@gmail.com>
2022-07-26 10:14:15 -0500

    Cambio hero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
- 
+import hero from './img/hero.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <h1>Hola Carlos</h1>
+     <img src={hero} alt="hero"/>
     </div>
   );
 }
